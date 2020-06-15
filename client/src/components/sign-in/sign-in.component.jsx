e1aad0d2d27a2db323726c8ff79de9dbb7083d27
liviuschera <liviu115@gmail.com>
2020-06-15 12:17:40 +1000

    fixed signup login issues

--- client/src/components/sign-in/sign-in.component.jsx
+++ client/src/components/sign-in/sign-in.component.jsx
@@ -8,57 +8,57 @@ import { postData } from '../../utils/connect-to-api';
 import CurrentUserContext from '../../contexts/current-user.context';
 
 export default function SignIn() {
-  const { setCurrentUser } = useContext(CurrentUserContext);
+   const { setCurrentUser } = useContext(CurrentUserContext);
 
-  const [state, setState] = useState({
-    email: '',
-    password: '',
-  });
+   const [state, setState] = useState({
+      email: '',
+      password: '',
+   });
 
-  async function handleSubmit(event) {
-    event.preventDefault();
-    const getUser = await postData('http://localhost:5000/signin', state);
+   async function handleSubmit(event) {
+      event.preventDefault();
+      const getUser = await postData('http://localhost:5000/signin', state);
 
-    if (getUser?.id) {
-      setCurrentUser(getUser);
-    }
-  }
+      if (getUser?.id) {
+         setCurrentUser(getUser);
+      }
+   }
 
-  function handleInputChange(event) {
-    const { name, value } = event.target;
+   function handleInputChange(event) {
+      const { value, name } = event.target;
 
-    setState((prevState) => {
-      const newState = {
-        ...prevState,
-        ...{ [name]: value },
-      };
-      return newState;
-    });
-  }
+      setState((prevState) => {
+         const newState = {
+            ...prevState,
+            ...{ [name]: value },
+         };
+         return newState;
+      });
+   }
 
-  return (
-    <SignInContainer>
-      <Title>Sign In with your email and password</Title>
-      <form action="post" method="post" onSubmit={handleSubmit}>
-        <FormInput
-          name="email"
-          type="email"
-          label="Email"
-          onChange={handleInputChange}
-          value={state.email}
-        />
-        <FormInput
-          name="password"
-          type="password"
-          label="Password"
-          onChange={handleInputChange}
-          value={state.password}
-        />
-        <CustomButton type="submit">Sign In</CustomButton>
-        <CustomButton isLink={true} to="/signup">
-          Register
-        </CustomButton>
-      </form>
-    </SignInContainer>
-  );
+   return (
+      <SignInContainer>
+         <Title>Sign In with your email and password</Title>
+         <form action="post" method="post" onSubmit={handleSubmit}>
+            <FormInput
+               name="email"
+               type="email"
+               label="Email"
+               onChange={handleInputChange}
+               value={state.email}
+            />
+            <FormInput
+               name="password"
+               type="password"
+               label="Password"
+               onChange={handleInputChange}
+               value={state.password}
+            />
+            <CustomButton type="submit">Sign In</CustomButton>
+            <CustomButton isLink={true} to="/signup">
+               Register
+            </CustomButton>
+         </form>
+      </SignInContainer>
+   );
 }
--- client/src/components/sign-up/sign-up.component.jsx
+++ client/src/components/sign-up/sign-up.component.jsx
@@ -6,80 +6,80 @@ import CustomButton from '../custom-button/custom-button.component';
 import { postData } from '../../utils/connect-to-api';
 
 export default function SignUp(props) {
-  const [state, setState] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
-  });
+   const [state, setState] = useState({
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+   });
 
-  async function handleSubmit(event) {
-    event.preventDefault();
-    try {
-      const response = await postData('http://localhost:5000/signup', state);
-      props.history.push('/');
-      console.log(response);
-    } catch (error) {
-      console.error('Sign-up client error: ', error);
-    }
-  }
+   async function handleSubmit(event) {
+      event.preventDefault();
+      try {
+         const response = await postData('http://localhost:5000/signup', state);
+         props.history.push('/');
+         console.log(response);
+      } catch (error) {
+         console.error('Sign-up client error: ', error);
+      }
+   }
 
-  function handleInputChange(event) {
-    const { name, value } = event.target;
+   function handleInputChange(event) {
+      const { value, name } = event.target;
 
-    setState((prevState) => {
-      const newState = { ...prevState, ...{ [name]: value } };
-      return newState;
-    });
-  }
+      setState((prevState) => {
+         const newState = { ...prevState, ...{ [name]: value } };
+         return newState;
+      });
+   }
 
-  return (
-    <SignInContainer>
-      <Title>Sign up with your email and password</Title>
-      <form method="post" onSubmit={handleSubmit}>
-        <FormInput
-          name="firstName"
-          type="text"
-          label="First Name"
-          value={state.firstName}
-          onChange={handleInputChange}
-          required
-        />
-        <FormInput
-          name="lastName"
-          type="text"
-          label="Last Name"
-          value={state.lastName}
-          onChange={handleInputChange}
-          required
-        />
-        <FormInput
-          name="email"
-          type="email"
-          label="Email"
-          value={state.email}
-          onChange={handleInputChange}
-          required
-        />
-        <FormInput
-          name="password"
-          type="password"
-          label="Password"
-          value={state.password}
-          onChange={handleInputChange}
-          required
-        />
-        <FormInput
-          name="confirmPassword"
-          type="password"
-          label="Confirm password"
-          value={state.confirmPassword}
-          onChange={handleInputChange}
-          required
-        />
-        <CustomButton type="submit">Register</CustomButton>
-      </form>
-    </SignInContainer>
-  );
+   return (
+      <SignInContainer>
+         <Title>Sign up with your email and password</Title>
+         <form method="post" onSubmit={handleSubmit}>
+            <FormInput
+               name="firstName"
+               type="text"
+               label="First Name"
+               value={state.firstName}
+               onChange={handleInputChange}
+               required
+            />
+            <FormInput
+               name="lastName"
+               type="text"
+               label="Last Name"
+               value={state.lastName}
+               onChange={handleInputChange}
+               required
+            />
+            <FormInput
+               name="email"
+               type="email"
+               label="Email"
+               value={state.email}
+               onChange={handleInputChange}
+               required
+            />
+            <FormInput
+               name="password"
+               type="password"
+               label="Password"
+               value={state.password}
+               onChange={handleInputChange}
+               required
+            />
+            <FormInput
+               name="confirmPassword"
+               type="password"
+               label="Confirm password"
+               value={state.confirmPassword}
+               onChange={handleInputChange}
+               required
+            />
+            <CustomButton type="submit">Register</CustomButton>
+         </form>
+      </SignInContainer>
+   );
 }
